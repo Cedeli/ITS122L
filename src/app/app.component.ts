3a0042c2c5d3175390857ae235372ce0e8e88dc9
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-03-07 18:44:01 +0800

    Create initial home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'BRMO';
