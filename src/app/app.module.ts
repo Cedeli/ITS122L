43302db3bf899f9e3688f6280556b082d5bf2997
Prostic <140481101+Prosticc@users.noreply.github.com>
2025-03-08 16:27:44 +0800

    a
    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+// Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component.ts';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
