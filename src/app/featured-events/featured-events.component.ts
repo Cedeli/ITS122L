e3b17a38baec5e16a9efbd32d24ac3df5e793377
Prostic <140481101+Prosticc@users.noreply.github.com>
2025-03-08 18:37:44 +0800

    Update featured-events.component.ts
    yep

--- src/app/featured-events/featured-events.component.ts
+++ src/app/featured-events/featured-events.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-featured-events',
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   templateUrl: './featured-events.component.html',
   styleUrls: ['./featured-events.component.scss']
 })
-export class FeaturedEventsComponent {
+export class FeaturedEventsComponent implements OnInit {
+  featuredEvents: Array<{ id: number, title: string, date: string, description: string, imageUrl: string }> = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.featuredEvents = [
+      {
+        id: 1,
+        title: 'Rosary Month',
+        date: 'March 2025',
+        description: 'description lorem',
+        imageUrl: 'placeholder.png'
+      },
+      {
+        id: 2,
+        title: 'Community Outreach',
+        date: 'March 7, 2025',
+        description: 'amongusmaongus',
+        imageUrl: 'placeholder.png'
+      },
+      {
+        id: 3,
+        title: 'Annual General Meeting',
+        date: 'March 7, 2025',
+        description: 'ogmasudsay',
+        imageUrl: 'placeholder.png'
+      }
+    ];
+  }
 }
