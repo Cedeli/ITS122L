1d8c402e09481d4c6c8c841dbf88ccc92ad34a0d
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-03-08 19:22:50 +0800

    Create auth service and log in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
 import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, HomeComponent],
+  imports: [RouterOutlet, NgOptimizedImage, HomeComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
