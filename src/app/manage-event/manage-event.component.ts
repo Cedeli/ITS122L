fd25934ad8bfc9cc68b4f48a574b7f4d528f5f70
OverLuked <edillorluke@gmail.com>
2025-03-13 02:51:58 +0800

    Add event management template

--- src/app/manage-event/manage-event.component.ts
+++ src/app/manage-event/manage-event.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AdminComponent } from '../admin/admin.component';
 
 @Component({
   selector: 'app-manage-event',
   imports: [
     CommonModule,
     FormsModule,
-    AdminComponent
   ],
   templateUrl: './manage-event.component.html',
   styleUrls: ['./manage-event.component.scss']
