55e7ec46e04f7f04c9bce3d123f3edb883e77f0a
OverLuked <edillorluke@gmail.com>
2025-03-13 01:03:45 +0800

    Add eventComponent in app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { EventComponent } from './event/event.component';
 import { AnnouncementsComponent } from './announcements/announcements.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './services/auth.guard';
@@ -11,7 +12,7 @@ const routeConfig: Routes = [
   { path: 'home', component: HomeComponent, title: 'Home', canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'announcements', component: AnnouncementsComponent, canActivate: [AuthGuard] },
-  { path: 'events',  component: HomeComponent,  title: 'Events', canActivate: [AuthGuard] },
+  { path: 'events',  component: EventComponent,  title: 'Events', canActivate: [AuthGuard] },
   { path: 'contact',  component: HomeComponent,  title: 'Contact', canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, title: 'Login', },
   { path: 'register', component: RegisterComponent, title: 'Register', },
