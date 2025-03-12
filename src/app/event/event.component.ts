c4abcdd08d10ba729e9eeb1e1d0a4641ceb863fc
OverLuked <edillorluke@gmail.com>
2025-03-12 23:13:10 +0800

    Add filter and previous event section

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -11,20 +11,54 @@ export class EventComponent {
     {
       id: 1,
       title: 'Event Title #1',
+      date: '03/12/2025',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
       imgsrc: '/placeholder.png'
     },
     {
       id: 2,
       title: 'Event Title #2',
+      date: '03/12/2025',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
       imgsrc: '/placeholder.png'
     },
     {
       id: 3,
       title: 'Event Title #3',
+      date: '03/12/2025',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
+      imgsrc: '/placeholder.png'
+    },
+    {
+      id: 4,
+      title: 'Event Title #3',
+      date: '03/12/2025',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
       imgsrc: '/placeholder.png'
     },
   ];
+
+  public eventHistory = [
+    {
+      id: 1,
+      title: 'Event Title #1',
+      date: '03/12/2024',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
+      imgsrc: '/placeholder.png'
+    },
+    {
+      id: 2,
+      title: 'Event Title #2',
+      date: '03/12/2024',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
+      imgsrc: '/placeholder.png'
+    },
+    {
+      id: 3,
+      title: 'Event Title #3',
+      date: '03/12/2024',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
+      imgsrc: '/placeholder.png'
+    },
+  ]
 }
