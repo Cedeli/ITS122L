cbae5be6ba9fbd6574a5b31d2048d9fd766d75f6
OverLuked <edillorluke@gmail.com>
2025-03-13 02:51:04 +0800

    Add admin content and route for child components

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
