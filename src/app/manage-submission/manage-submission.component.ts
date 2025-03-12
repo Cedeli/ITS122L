479e42f84e5e2550ac5f8892c6c335f504adca54
OverLuked <edillorluke@gmail.com>
2025-03-13 02:52:39 +0800

    Add manage submission template

--- src/app/manage-submission/manage-submission.component.ts
+++ src/app/manage-submission/manage-submission.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { AdminComponent } from '../admin/admin.component';
 
 @Component({
   selector: 'app-manage-submission',
   imports: [
     CommonModule,
     FormsModule,
-    AdminComponent,
+
   ],
   templateUrl: './manage-submission.component.html',
   styleUrls: ['./manage-submission.component.scss']
