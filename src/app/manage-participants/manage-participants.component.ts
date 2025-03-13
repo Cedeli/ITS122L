294f72e81f1f15463298969047af1fcd74aab2a6
OverLuked <edillorluke@gmail.com>
2025-03-14 01:47:12 +0800

    Initial commit

---
+++ src/app/manage-participants/manage-participants.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-participants',
+  imports: [],
+  templateUrl: './manage-participants.component.html',
+  styleUrl: './manage-participants.component.scss'
+})
+export class ManageParticipantsComponent {
+
+}
