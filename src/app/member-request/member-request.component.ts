6d57dc89acf387eec1392d2f37845dacae25e5ba
OverLuked <edillorluke@gmail.com>
2025-03-14 03:27:29 +0800

    Add membership request dialog

---
+++ src/app/member-request/member-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-request',
+  imports: [],
+  templateUrl: './member-request.component.html',
+  styleUrl: './member-request.component.scss'
+})
+export class MemberRequestComponent {
+
+}
