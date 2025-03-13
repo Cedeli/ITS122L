9c6a15ed26605298bf2d538ce67068285f88e13c
Prostic <140481101+Prosticc@users.noreply.github.com>
2025-03-13 14:07:55 +0800

    has problems
    problems with cyclic dependency

--- src/app/members-list/members-list.component.ts
+++ src/app/members-list/members-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-// import { AngularFirestore } from '@angular/fire/compat/firestore';
-// import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
-interface Member {
+interface User {
   name: string;
   email: string;
 }
 
 @Component({
   selector: 'app-members-list',
+  standalone: true,
   imports: [
     CommonModule,
   ],
@@ -17,13 +18,17 @@ interface Member {
   styleUrls: ['./members-list.component.scss']
 })
 export class MembersListComponent implements OnInit {
-  members: Member[] = [
-    { name: 'John Doe', email: 'email@.com' },
-  ];
+  users: User[] = [];
 
-  constructor() {}
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
+  getUsers(): void {
+    this.firestore.collection<User>('users').valueChanges().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
