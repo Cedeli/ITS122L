dc21a6674f5d0c27d3b8cedfef5b3b1a609b7ccf
Prostic <140481101+Prosticc@users.noreply.github.com>
2025-03-13 22:52:18 +0800

    changed events info

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -15,29 +15,29 @@ export class EventComponent {
       id: 1,
       title: 'Event Title #1',
       date: '03/12/2025',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
     {
       id: 2,
       title: 'Event Title #2',
       date: '03/12/2025',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
     {
       id: 3,
       title: 'Event Title #3',
       date: '03/12/2025',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
     {
       id: 4,
       title: 'Event Title #3',
       date: '03/12/2025',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
   ];
 
@@ -46,22 +46,22 @@ export class EventComponent {
       id: 1,
       title: 'Event Title #1',
       date: '03/12/2024',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
     {
       id: 2,
       title: 'Event Title #2',
       date: '03/12/2024',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
     {
       id: 3,
       title: 'Event Title #3',
       date: '03/12/2024',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?',
-      imgsrc: '/placeholder.png'
+      description: 'To be added by admin',
+      imgsrc: '/BRM Logo.jpg'
     },
   ];
 
