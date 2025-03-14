f54397dcdbab25b8b1fa9831caf91a36cc727422
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-03-15 04:25:32 +0800

    Environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDrlVyTCk2MImhWZEckPPkyNJpJa3Hpja0",
+    authDomain: "brm-organization-website.firebaseapp.com",
+    projectId: "brm-organization-website",
+    storageBucket: "brm-organization-website.firebasestorage.app",
+    messagingSenderId: "126186357660",
+    appId: "1:126186357660:web:c8a3158c41739c9ef77e59",
+    measurementId: "G-VPK809C3FW"
+  }
+};
