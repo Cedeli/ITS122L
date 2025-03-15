ea7f6cc9be717c53d04d89ba1f92446206b26c77
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-03-15 17:04:17 +0800

    deployment 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: String(process.env['FIREBASE_API_KEY']),
-    authDomain: String(process.env['FIREBASE_AUTH_DOMAIN']),
-    projectId: String(process.env['FIREBASE_PROJECT_ID']),
-    storageBucket: String(process.env['FIREBASE_STORAGE_BUCKET']),
-    messagingSenderId: String(process.env['FIREBASE_MESSAGING_SENDER_ID']),
-    appId: String(process.env['FIREBASE_APP_ID']),
-    measurementId: String(process.env['FIREBASE_MEASUREMENT_ID'])
+    apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+    authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
+    projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
+    storageBucket: "FIREBASE_STORAGE_BUCKET_PLACEHOLDER",
+    messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER",
+    appId: "FIREBASE_APP_ID_PLACEHOLDER",
+    measurementId: "FIREBASE_MEASUREMENT_ID_PLACEHOLDER"
   }
 };
